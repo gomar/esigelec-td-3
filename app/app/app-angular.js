275c0b832e65d900a6bf0df3e7f248e9f12db7a0
Adrien Gomar <adrien.gomar@bearingpoint.com>
2017-11-09 22:24:47 +0100

    Init

---
+++ app/app/app-angular.js
@@ -0,0 +1,62 @@
+var myApp = angular.module('app', []);
+
+
+myApp.service('BlockchainService', function($rootScope, $http) {
+
+    var blockchainService = {};
+
+    // initializing some global variables
+    deployedContract = MetaCoin.deployed();
+    accounts = window.web3.eth.accounts;
+
+    blockchainService.getAccounts = function() {
+        return accounts;
+    }
+
+    blockchainService.refreshBalance = function(from_account) {
+        // getBalance function is a promise. Since we have to call it
+        // once per account, we better store all the promise in an array
+        // and resolve them using Promise.all()
+        // This final promise is return so that the final function only has
+        // to make one promise
+        var promises = [];
+        for (var i = 0; i < accounts.length; i++) {
+            promises.push(
+                deployedContract.getBalance.call(accounts[i], {from: from_account}).then( function(value){
+                    return value.toNumber();
+                })
+            );
+        }
+        return Promise.all(promises).then(function(values){
+            balances = {};
+            for (var i = 0; i < values.length; i++) {
+                balances[accounts[i]] = values[i];
+            }
+            return balances;
+        });
+    };
+
+    blockchainService.sendCoin = function(to_address, amount) {
+        return deployedContract.sendCoin(to_address, amount, {from: accounts[0]});
+    }
+
+    return blockchainService;
+})
+
+
+myApp.controller('blockchainController', function (BlockchainService, $scope, $rootScope) {
+    $scope.accounts = BlockchainService.getAccounts();
+
+    $scope.ctrlRefreshBalance = function(from_account) {
+        BlockchainService.refreshBalance(from_account).then(function(value) {
+            $scope.balances = value;
+            $scope.$apply();
+        });
+    };
+
+    $scope.ctrlSendCoin = function(to_address, amount) {
+        BlockchainService.sendCoin(to_address, amount).then(function() {
+            $scope.ctrlRefreshBalance($scope.accounts[0]);
+        })
+    }
+});
